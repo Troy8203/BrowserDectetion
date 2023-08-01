117d5121972f829f5f4f3a0eb3cb96e085b067b7
Roy Ayala <troysg8203@gmail.com>
2023-08-01 13:55:26 -0400

    base browser detection

---
+++ main.js
@@ -0,0 +1,16 @@
+let userAgent = navigator.userAgent;
+let browserName;
+if (userAgent.match(/edg/i)) {
+    browserName = "edge";
+} else if (userAgent.match(/chrome|chromium|crios/i)) {
+    browserName = "chrome";
+} else if (userAgent.match(/firefox|fxios/i)) {
+    browserName = "firefox";
+} else if (userAgent.match(/safari/i)) {
+    browserName = "safari";
+} else {
+  browserName = "No browser detection";
+}
+
+let element = document.querySelector(`#${browserName}`);
+element.style.filter = "grayscale(0%)";
